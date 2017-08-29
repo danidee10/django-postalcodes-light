60a74524ce0a91c32461b4d562c1077f59ebd712
Tobias McNulty <tobias.mcnulty@gmail.com>
2017-08-28 22:55:38 -0400

    map field names more directly to underlying data and update corresponding migration and admin views

--- postalcodes_light/models.py
+++ postalcodes_light/models.py
@@ -1,4 +1,4 @@
-from django.contrib.gis.db import models
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -9,13 +9,15 @@ class PostalCode(models.Model):
     Originally based on the Census Bureau's zip code data
     http://www.census.gov/geo/www/gazetteer/gazetteer2010_layout.html#zcta
     """
-    code = models.CharField(_("postal code"), max_length=20)
-    country = models.CharField(_("country"), max_length=2)
-    city = models.CharField(_("city"), max_length=200, blank=True, null=True)
-    state = models.CharField(_("state"), max_length=100, blank=True, null=True)
-    location = models.PointField(null=True, blank=True)
-
-    objects = models.GeoManager()
+    country_code = models.CharField(_("country_code"), max_length=2)
+    postal_code = models.CharField(_("postal_code"), max_length=20)
+    place_name = models.CharField(_("place_name"), max_length=200, blank=True, null=True)
+    admin_name1 = models.CharField(_("admin_name1"), max_length=100, blank=True, null=True)
+    admin_code1 = models.CharField(_("admin_code1"), max_length=20, blank=True, null=True)
+    admin_name2 = models.CharField(_("admin_name2"), max_length=100, blank=True, null=True)
+    admin_code2 = models.CharField(_("admin_code2"), max_length=20, blank=True, null=True)
+    admin_name3 = models.CharField(_("admin_name3"), max_length=100, blank=True, null=True)
+    admin_code3 = models.CharField(_("admin_code3"), max_length=20, blank=True, null=True)
 
     class Meta:
         verbose_name = _("Postal code")
